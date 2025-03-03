1e62d069c1f6c0c2e8a56787f1f56c6156f135f0
Faris Mujakić <faris.mujakic@gmail.com>
2025-03-03 19:49:36 +0100

    Rollup w TS, @gjsify/types

--- run-nested-shell.sh
+++ run-nested-shell.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -e
 
-export G_MESSAGES_DEBUG=all
+# export G_MESSAGES_DEBUG=all
 export MUTTER_DEBUG_DUMMY_MODE_SPECS=1600x900
-export SHELL_DEBUG=all
+# export SHELL_DEBUG=all
 
 dbus-run-session -- \
     gnome-shell --nested \
